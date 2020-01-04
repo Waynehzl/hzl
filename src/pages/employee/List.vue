0527f2904480f7b49f72d16ecfe8453e0eeeb73b
Waynehzl <936474981@qq.com>
2020-01-04 09:24:07 +0800

    all commit

--- src/pages/employee/List.vue
+++ src/pages/employee/List.vue
@@ -139,9 +139,9 @@ export default {
         toDeleteHandler(id){
         
              this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
         }).then(() => {
           this.$message({
             type: 'success',
